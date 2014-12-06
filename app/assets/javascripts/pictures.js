e4b00f4850bbed46c177a395946b27392e8d9fad
Arnaud Lenglet <arlenglet@gmail.com>
2014-12-06 22:31:58 +0100

    feat(Pictures): add new thumb photos + shufflejs working!

---
+++ app/assets/javascripts/pictures.js
@@ -0,0 +1,55 @@
+//= require modernizr
+//= require shufflejs
+
+// Overrideable options
+Shuffle.options = {
+  group: "preparation", // Initial filter group.
+  speed: 250, // Transition/animation speed (milliseconds).
+  easing: 'ease-out', // CSS easing function to use.
+  itemSelector: '', // e.g. '.picture-item'.
+  sizer: null, // Sizer element. Use an element to determine the size of columns and gutters.
+  gutterWidth: '', // A static number or function that tells the plugin how wide the gutters between columns are (in pixels).
+  columnWidth: '', // A static number or function that returns a number which tells the plugin how wide the columns are (in pixels).
+  delimeter: null, // If your group is not json, and is comma delimeted, you could set delimeter to ','.
+  buffer: 0, // Useful for percentage based heights when they might not always be exactly the same (in pixels).
+  initialSort: null, // Shuffle can be initialized with a sort object. It is the same object given to the sort method.
+  // throttle: throttle, // By default, shuffle will throttle resize events. This can be changed or removed.
+  throttleTime: 300, // How often shuffle can be called on resize (in milliseconds).
+  sequentialFadeDelay: 150, // Delay between each item that fades in when adding items.
+  // supported: CAN_TRANSITION_TRANSFORMS // Whether to use transforms or absolute positioning.
+};
+
+$(document).ready(function() {
+  var $grid = $('#grid'),
+  		$filterOptions = $('.filter-options'),
+      $sizer = $grid.find('.shuffle__sizer');
+
+  $grid.shuffle({
+    itemSelector: '.picture-item',
+    sizer: $sizer
+  });
+
+  setupFilters($grid, $filterOptions);
+});
+
+// Set up button clicks
+setupFilters = function($grid, $filterOptions) {
+  var $btns = $filterOptions.children();
+  $btns.on('click', function() {
+    var $this = $(this),
+        isActive = $this.hasClass( 'active' ),
+        group = isActive ? 'all' : $this.data('group');
+
+    // Hide current label, show current label in title
+    if ( !isActive ) {
+      $('.filter-options .active').removeClass('active');
+    }
+
+    $this.toggleClass('active');
+
+    // Filter elements
+    $grid.shuffle( 'shuffle', group );
+  });
+
+  $btns = null;
+};
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,6 +2,11 @@ class PicturesController < ApplicationController
 	before_action :is_authenticated!
 
   def index
+  	@preparation_images = Array 1..13
+  	@cityhall_images = Array 14..24
+  	@church_images = Array 25..68
+  	@shooting_images = Array 69..86
+  	@night_images = Array 87..155
   end
 
   def show
