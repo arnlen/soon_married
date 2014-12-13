f2eb33caf6a9f97a2abe50a6c546fe5430cfcede
Arnaud Lenglet <arlenglet@gmail.com>
2014-12-13 16:40:36 +0100

    feat(Responsive): upgrade response for login page

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -6,35 +6,10 @@
 
 $(function() {
 
-
 // ---------------------------------------------
 // PAGE LOADING AND INIT
 
-    // Load the music
-    // Old HTML: <%= audio_tag 'Nat_King_Cole_LOVE.wav', autoplay: true %>
-    var musicLOVE = new Audio(),
-        musicLoaded = false;
-
-    // Begin music loading
-    console.log('Musics loading has started...');
-    musicLOVE.src = 'audios/Nat_King_Cole_LOVE.ogg';
-
-    musicLOVE.addEventListener('canplay', function() {
-        console.log('... Musics loaded!');
-        musicLoaded = true;
-        musicLOVE.play();
-        $('#loading_overlay').hide();
-        $('#home_container').show();
-    });
-
-    // Fallback if music can't be loaded
-    setTimeout(function() {
-        if (musicLoaded != true) {
-            console.log('... Musics not loaded, fallback.');
-            $('#loading_overlay').hide();
-            $('#home_container').show();
-        }
-    }, 500);
+    console.log($('#countdown_container').attr('style'));
 
     // Initiates dates
     var dDay = new Date(2014, 7, 15, 14, 0, 0),
@@ -51,64 +26,4 @@ $(function() {
 
     countdown.setTime(timeLeft);
     countdown.setCountdown(false);
-
-    // Hide hidden (hiddable ?) fields (lol) at page load
-    $('#guest_will_be_present').hide();
-
-
-// ---------------------------------------------
-// BUTTONS ANIMATIONS
-
-    $('button').hover(
-        function() {
-            $(this).addClass('beating');
-        },
-        function() {
-            $(this).removeClass('beating');
-        }
-    );
-
-
-// ---------------------------------------------
-// COMING YES/NO BUTTONS
-
-    // Buttons coming from up on page load
-    $('#coming_buttons').removeClass('up');
-
-    // Buttons + form animation on click
-    $('.yes, .no').on('click', function() {
-        $('#coming_buttons').addClass('down');
-        $('#form_container').removeClass('up');
-    });
-
-    // YES I'll come!
-    $('#will_be_present').on('click', function() {
-        $('#guest_will_be_present').val('true');
-        $('label').text("Ton adresse nous permettra de t'envoyer un mail de rappel à quelques jours de l'événement !");
-    });
-
-    // NO I won't come...
-    $('#wont_be_present').on('click', function() {
-        $('#guest_will_be_present').val('false');
-        $('label').text("Laisse-nous tes coordonnées, juste au cas où !");
-    });
-
-
-// ---------------------------------------------
-// CONFIRMATION / CANCEL
-
-    // Cancel button: restore previous "Yes/No" buttons
-    $('.cancel').on('click', function() {
-        $('#coming_buttons').removeClass('down');
-        $('#form_container').addClass('up');
-    });
-
-    $("#new_guest").on("ajax:success", function(e, data, status, xhr) {
-        $('#success_confirmation').removeClass('up');
-        $('#form_container').addClass('down');
-    });
-
-    $("#new_guest").on("ajax:error", function(e, data, status, xhr) {
-        console.log(e, data, status, xhr);
-    });
 });
